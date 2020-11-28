b99cbabf20d2c1664f2cd52afbed52d8776f0c6b
Olivier Daigle <ophd@pm.me>
2020-11-28 13:31:28 -0500

    Adds regularization on backward propagation for dense layer

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -22,12 +22,29 @@ class Layer_Dense:
         ''' forward pass '''
         self.inputs = inputs
         self.output = np.dot(inputs, self.weights) + self.biases
+        
     def backward(self, dvalues):
         ''' backward propagation.
             input: dvalues  the gradient of the forward layer of neurons
         '''
         self.dweights = np.dot(self.inputs.T, dvalues)
         self.dbiases = np.sum(dvalues, axis=0, keepdims=True)
+
+        # Gradients on regularization
+        if self.weight_regularizer_l1 > 0:
+            dL1 = np.ones_like(self.weights)
+            dL1[self.weights <0] = -1
+            self.dweights += self.weight_regularizer_l1 * dL1
+        if self.weight_regularizer_l2 > 0:
+            self.dweights += 2 * self.weight_regularizer_l2 * self.weights
+        
+        if self.bias_regularizer_l1 > 0:
+            dL1 = np.ones_like(self.biases)
+            dL1[self.biases < 0] = -1
+            self.dbiases += self.bias_regularizer_l1 * dL1
+        if self.bias_regularizer_l2 > 0:
+            self.dbiases += 2 * self.bias_regularizer_l2 * self.biases
+        
         # create the gradient for this layer
         self.dinputs = np.dot(dvalues, self.weights.T)
 
