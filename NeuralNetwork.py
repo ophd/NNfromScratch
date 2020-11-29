1735223af6c282bec784a79070ea3e40a80b1bd9
Olivier Daigle <ophd@pm.me>
2020-11-29 17:09:47 -0500

    Adds binary logistic regression

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -48,6 +48,23 @@ class Layer_Dense:
         # create the gradient for this layer
         self.dinputs = np.dot(dvalues, self.weights.T)
 
+class Layer_Dropout:
+    def __init__(self, rate):
+        ''' input is dropout rate, but it is saved in the layer
+            as the retention rate
+        '''
+        self.rate = 1 - rate
+    
+    def forward(self, inputs):
+        self.inputs = inputs
+        self.binary_mask = np.random.binomial(1, self.rate, size=inputs.shape) \
+                           / self.rate
+        self.output = inputs * self.binary_mask
+    
+    def backward(self, dvalues):
+        self.dinputs = dvalues * self.binary_mask
+
+
 class Activation_ReLU:
     ''' Rectified Linear Unit Activation Function '''
     def __init__(self):
@@ -76,6 +93,15 @@ class Activation_Softmax:
             jacobian_matrix = np.diagflat(single_output) - \
                               np.dot(single_output, single_output.T)
             self.dinputs[i] = np.dot(jacobian_matrix, single_dvalues)
+
+class Activation_Sigmoid:
+    def forward(self, inputs):
+        self.inputs = inputs
+        self.outputs = 1 / (1 + np.exp(-inputs))
+    
+    def backward(self, dvalues):
+        self.dinputs = dvalues * (1 - self.output) * self.output
+
 
 class Activation_Softmax_Loss_CategoricalCrossEntropy():
     def __init__(self):
@@ -157,6 +183,30 @@ class Loss_CategoricalCrossEntropy(Loss):
             y_true = np.eye(no_of_labels)[y_true]
         # calculate & normalize gradient
         self.dinputs = (-y_true / dvalues) / no_of_samples
+
+
+class Loss_BinaryCrossEntropy(Loss):
+    def forward(self, y_pred, y_true):
+        # clip data to avoid division by zero
+        y_pred_clip = np.clip(y_pred, 1e-7, 1 - 1e-7)
+
+        sample_losses = -(y_true * np.log(y_pred_clip) +
+                          (1 - y_true) * np.log(1 - y_pred_clip))
+        sample.losses = np.mean(sample_losses, axis=-1)
+    
+    def backward(self, dvalues, y_true):
+        no_of_samples = len(dvalues)
+        no_of_outputs = len(dvalues[0])
+
+        # clip data to avoid division by zero
+        clipped_dvalues = np.clip(dvalues, 1e-7, 1 - 1e-7)
+
+        # Calculate the gradient
+        self.dinputs = -(y_true / clipped_dvalues) -
+                        (1 - y_true) / (1 - clipped_dvalues)) / no_of_outputs
+        # Normalize the gradient
+        self.dinputs = self.dinputs / no_of_samples
+
 
 class Optimizer_SGD:
     def __init__(self, learning_rate=1.0, decay=0.0, momentum=0.):
@@ -338,42 +388,62 @@ class Optimizer_Adam:
 
 
 if __name__ == '__main__':
-    X, y = spiral_data(100, 3)
-
-    dense1 = Layer_Dense(2, 64)
-    dense2 = Layer_Dense(64, 3)
+    X, y = spiral_data(1000, 3)
+    
+    dense1 = Layer_Dense(2, 512, weight_regularizer_l2=5e-4,
+                         bias_regularizer_l2=5e-4)
     activation1 = Activation_ReLU()
+    dropout1 = Layer_Dropout(0.1)
+    dense2 = Layer_Dense(512, 3)
     loss_activation = Activation_Softmax_Loss_CategoricalCrossEntropy()
-    # optimizer = Optimizer_SGD(learning_rate=0.98, decay=1.0e-3, momentum=0.9)
-    # optimizer = Optimizer_Adagrad(learning_rate=0.9, decay=1.0e-4)
-    # optimizer = Optimizer_RMSprop(learning_rate=0.02, decay=1e-5, rho=0.999)
-    optimizer = Optimizer_Adam(learning_rate=0.05, decay=5e-7)
-
+    optimizer = Optimizer_Adam(learning_rate=0.05, decay=5e-5)
+    
     for epoch in range(10001):
         dense1.forward(X)
         activation1.forward(dense1.output)
-        dense2.forward(activation1.output)
-        loss = loss_activation.forward(dense2.output, y)
-        loss += loss_activation.regularization_loss(dense1)
-        loss += loss_activation.regularization_loss(dense2)
+        dropout1.forward(activation1.output)
+
+        dense2.forward(dropout1.output)
+        
+        data_loss = loss_activation.forward(dense2.output, y)
+        regularization_loss = loss_activation.loss.regularization_loss(dense1) + \
+                              loss_activation.loss.regularization_loss(dense2)
+        
+        loss = data_loss + regularization_loss
 
         predictions = np.argmax(loss_activation.output, axis=1)
         if len(y.shape) == 2:
             y = np.argmax(y, axis=1)
         accuracy = np.mean(predictions == y)
-        if not epoch % 250:
-            print(f'epoch: {epoch}',
-                  f'\tacc: {accuracy:.3f}',
-                  f'\tloss: {loss:.3f}'
-                  f'\tlr: {optimizer.current_learning_rate:0.6f}')
 
-        #backward pass
+        if not epoch % 100:
+            print(f'epoch: {epoch}',
+                  f'acc: {accuracy:.3f}',
+                  f'loss: {loss:.3f} (',
+                  f'data loss: {data_loss:.3f}, ',
+                  f'reg loss: {regularization_loss:.3f})',
+                  f'lr: {optimizer.current_learning_rate}')
+        
         loss_activation.backward(loss_activation.output, y)
         dense2.backward(loss_activation.dinputs)
-        activation1.backward(dense2.dinputs)
+        dropout1.backward(dense2.dinputs)
+        activation1.backward(dropout1.dinputs)
         dense1.backward(activation1.dinputs)
 
         optimizer.pre_update_params()
         optimizer.update_params(dense1)
         optimizer.update_params(dense2)
         optimizer.post_update_params()
+    
+    # Model validation
+    X_test, y_test = spiral_data(samples=100, classes=3)
+    dense1.forward(X_test)
+    activation1.forward(dense1.output)
+    dense2.forward(activation1.output)
+    loss = loss_activation.forward(dense2.output, y_test)
+    predictions = np.argmax(loss_activation.output, axis=1)
+    if len(y_test.shape) == 2:
+        y_test = np.argmax(y_test, axis=1)
+    accuracy = np.mean(predictions == y_test)
+
+    print(f'validation, acc: {accuracy:.3f}, loss: {loss:.3f}')
