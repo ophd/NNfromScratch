88a4b41fb9ba53221ad1ba18823b838e46aa7449
Olivier Daigle <ophd@pm.me>
2021-01-03 11:05:21 -0500

    Correct preprocessing issues Needed to convert array to numpy array and give the correct shape to the reshape method.

--- preprocess_new_images.py
+++ preprocess_new_images.py
@@ -8,9 +8,13 @@ def load_new_images(path):
     imgs = []
 
     for file in files:
+        print(f'Loading {file}...')
         image_data = cv2.imread(os.path.join(path, file), cv2.IMREAD_GRAYSCALE)
         image_data = cv2.resize(image_data, (28, 28))
-        image_data = image_data.reshape(1, -1).astype(np.float32)
+        image_data = 255 - image_data
         imgs.append(image_data)
+
+    imgs = np.array(imgs)
+    imgs = (imgs.reshape(imgs.shape[0], -1).astype(np.float32) - 127.5) / 127.5
 
     return imgs
